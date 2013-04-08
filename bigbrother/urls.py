f1352540104aab6476cc45419f766da992aa35f9
Anders Petersson <me@anderspetersson.se>
2013-04-08 22:46:06 +0200

    Fix import path in urls.py to work with django 1.5.

--- bigbrother/urls.py
+++ bigbrother/urls.py
@@ -1,8 +1,8 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url
 import bigbrother.views
 
-urlpatterns = patterns('',
+urlpatterns = patterns('url',
     url('^$', bigbrother.views.BigBrotherIndexView.as_view(), name='bigbrother_index'),
     url('^graph/(?P<slug>[^/]+)/$', bigbrother.views.BigBrotherGraphView.as_view(), name='bigbrother_graph'),
     url('^update/$', bigbrother.views.BigBrotherUpdateView.as_view(), name='bigbrother_update'),
