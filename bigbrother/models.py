550d709b7a95a57118bdc40b401945f61acff621
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-02-10 14:48:06 +0530

    Updated to support django 2+ and breaking support for django 1+

--- bigbrother/models.py
+++ bigbrother/models.py
@@ -4,3 +4,6 @@ class ModuleStat(models.Model):
     modulename = models.CharField('Module', max_length=64)
     added = models.DateTimeField('Stat Date', auto_now_add=True)
     value = models.DecimalField('Value', max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.modulename
