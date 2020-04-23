58dec2ba059b52b787772bdca281e3f51cc71261
Manoj Tyagi <tyagimanojtyagi.22@gmail.com>
2020-04-24 01:16:47 +0530

    Changed url() to path() in docs/installation.rst and Added Meta class in models

--- bigbrother/models.py
+++ bigbrother/models.py
@@ -5,5 +5,10 @@ class ModuleStat(models.Model):
     added = models.DateTimeField('Stat Date', auto_now_add=True)
     value = models.DecimalField('Value', max_digits=10, decimal_places=2)
 
+    class Meta:
+        ordering = ('added',)
+        verbose_name = "Module Stat"
+        verbose_name_plural = 'Module Stat'
+
     def __str__(self):
         return self.modulename
